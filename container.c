f979e630fc01e22a2bf9749b02b9c92fae049943
materialofmouse <materialofmouse@mail.com>
2020-05-09 12:23:57 +0900

    chore: add print inside data

--- container.c
+++ container.c
@@ -18,31 +18,36 @@ int main(){
 	errno = 0;
 	
 	struct __user_cap_header_struct hdr = { 0 };
-	hdr.pid = 0;
+	hdr.pid = (int)pid;
 	hdr.version = _LINUX_CAPABILITY_VERSION_3;
 
 	struct __user_cap_data_struct data = { 0 };
 	data.permitted =  (1 << CAP_NET_RAW) | (1 << CAP_SYS_CHROOT) | (1 << CAP_SYS_ADMIN);
-	data.inheritable = data.permitted;
 	data.effective = data.permitted;
+	data.inheritable = data.permitted;
+	
 	if(capset(&hdr, &data) < 0){
 		perror("capset");
-		printf("errno:%d\n",errno);
 	}
+	printf("---------capset--------\n");
+	printf("pid:%d\n",hdr.pid);
 	printf("effective:%lld\n",data.effective);
 	printf("inheritable:%lld\n",data.inheritable);
 	printf("permitted:%lld\n",data.permitted);
-	hdr.pid = (int)getpid();
+	printf("-----------------------\n");
+	
 	if(capget(&hdr, &data) < 0){
 		perror("capget");
-		printf("errno:%d\n",errno);
 	}
+	printf("---------capget--------\n");
+	printf("pid:%d\n",hdr.pid);
 	printf("effective:%lld\n",data.effective);
 	printf("inheritable:%lld\n",data.inheritable);
 	printf("permitted:%lld\n",data.permitted);
+	printf("-----------------------\n");
 
 	int status;
-	if( unshare(UNSHARE_FLAGS) == -1){
+	if( unshare(UNSHARE_FLAGS) < 0){
 		perror("unshare");
 		return 1;
 	}
@@ -51,8 +56,8 @@ int main(){
     return 1;
   }
 	//make cgroup
-	if( access("/sys/fs/cgroup/container", F_OK) != 0){
-		if( mkdir("/sys/fs/cgroup/container", 0644) != 0){
+	if( access("/sys/fs/cgroup/container", F_OK) < 0){
+		if( mkdir("/sys/fs/cgroup/container", 0644) < 0){
 			perror("mkdir");
 			return 1;
 		}
@@ -61,7 +66,7 @@ int main(){
 	int fd;
 	//プロセスの登録
 	fd = open("/sys/fs/cgroup/container/cgroup.procs", O_WRONLY);
-	if( fd == -1){ perror("open"); return 1; }
+	if( fd < 0){ perror("open"); return 1; }
 	int _pid = getpid();
 	char buff[6];
 	snprintf(buff, 6 , "%d", _pid);
@@ -70,13 +75,13 @@ int main(){
 	
 	//サブシステムの登録
 	fd = open("/sys/fs/cgroup/cgroup.subtree_control", O_WRONLY);
-	if (fd == -1){ perror("subtree error"); return 1;}
+	if (fd < 0){ perror("subtree error"); return 1;}
 	write(fd, "+cpu", 5);
 	close(fd);
 	
 	//CPU制限
 	fd = open("/sys/fs/cgroup/container/cpu.max", O_WRONLY);
-	if (fd == -1){ perror("cpu open"); return 1; }
+	if (fd < 0){ perror("cpu open"); return 1; }
 	write(fd, "10000", 6);//このサーバーの場合
 	close(fd);
 
@@ -85,15 +90,15 @@ int main(){
 
 		printf("child process:%d\n",(int)getpid());
     sethostname("container",9);
-		if (chdir("/home/mouse/container/debian") != 0){
+		if (chdir("/home/mouse/container/debian") < 0){
 			perror("chdir");
 			return 1;
 		}
-		if(chroot("/home/mouse/container/debian") != 0){
+		if(chroot("/home/mouse/container/debian") < 0){
 			perror("chroot");
 			return 1;
 		}
-		if (mount("proc", "/proc", "proc", 0, NULL) != 0){
+		if (mount("proc", "/proc", "proc", 0, NULL) < 0){
 				perror("mount");
 				return 1;
 		}
@@ -102,7 +107,7 @@ int main(){
     perror("bash");
   }
 
-  printf("parrent process:%d\n",(int)pid);
+  printf("parrent process:%d\n",(int)getpid());
   if ((pid = waitpid(pid,&status,0)) < 0) {
     perror("wait");
     return 1;
