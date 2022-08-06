4867066202ca59253a1cda4cff63ce0bda7dab39
materialofmouse <materialofmouse@gmail.com>
2022-08-06 20:08:25 +0900

    add: some tools

--- tools/container_launch.py
+++ tools/container_launch.py
@@ -4,16 +4,19 @@ import subprocess
 
 if __name__=="__main__":
     container_state = False
+    container_pid = 0
     while True:
         controller = open("./config/controller_pid", 'r')
         pid = int(controller.readline())
         if container_state and pid == 0:
             print("container stop")
-            subprocess.run(["./network/network_delete"], stdout=subprocess.DEVNULL)
+            subprocess.run(["./network/network_delete", str(container_pid)], stdout=subprocess.DEVNULL)
             container_state = False
+            container_pid = 0
         elif not container_state and pid > 0:
             print("container launch")
-            subprocess.run(["./network/network_setup"], stdout=subprocess.DEVNULL)
+            subprocess.run(["./network/network_setup", str(pid)], stdout=subprocess.DEVNULL)
+            container_pid = pid
             container_state = True
         controller.close()
         time.sleep(1)
